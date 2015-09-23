c21936412e8994f5f55f2af473702eb1cdeb0f22
Nathan Hendrix <nathanhendrix@icloud.com>
2015-09-22 21:18:33 -0500

    hi

--- Graphing Calculator/src/Equation.java
+++ Graphing Calculator/src/Equation.java
@@ -1,4 +1,45 @@
+import java.util.List;
 
 public class Equation {
+	 
+	private int Max = 0;
+	private int Min = 0;
+	private String Eq = null;
+	
+	
+	public Equation(String Equ, int Ma, int Mi)
+	{
+		this.Eq = Equ;
+		this.Max = Ma;
+		this.Min = Mi;
+		
+	}
+	
+	public List<Integer> Solve ()
+	{
+		int y = 0;
+		for(int i = Min; i <= Max; i+=.01)
+		{
+			for(int s = 0; s < Eq.length(); s++)
+			{
+				if((Eq.substring(s, s+1).equals("X")))
+						{
+							if(s != 0)
+							{
+								if(!((Eq.substring(s-1, s)).equals("/")) && ((Eq.substring(s-1, s)).equals("+")) && ((Eq.substring(s-1, s)).equals("*")) && ((Eq.substring(s-1, s)).equals("(")) && ((Eq.substring(s-1, s)).equals("-")) && ((Eq.substring(s+1, s+2)).equals("^")))
+								{
+									int x = (Integer.parseInt(Eq.substring(s-1, s)));
+									y = x*i;
+								
+								}
+							}
+						}
+			}
+		}
+		return null;
+		
+	}
+	
+	
 
 }
